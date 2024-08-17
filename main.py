4c9712a5a9e2109a28628edec315a02164304d01
tranu <tranulovianos@gmail.com>
2024-08-17 21:13:04 +0100

    US States game + CSV v2

--- main.py
+++ main.py
@@ -16,4 +16,44 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# # print(temp_list)
+#
+# average = sum(temp_list) / len(temp_list)
+# # print(average)
+#
+# print(data["temp"].mean().round(2))  # round to 2 decimal places
+# print(data["temp"].max())
+#
+# # Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+#
+# # Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(round(monday_temp_F))
+#
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+
+
+
+
+
+
+
