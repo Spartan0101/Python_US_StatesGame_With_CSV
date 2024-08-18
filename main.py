ba9e033049c9ccecef0e37ffc351c0a958c43cb3
tranu <tranulovianos@gmail.com>
2024-08-18 10:10:53 +0100

    US States game + CSV v3 CSV handling

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 #Pandas library for Data
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(data["temp"])
 
 # data_dict = data.to_dict()
@@ -44,15 +44,28 @@ data = pandas.read_csv("weather_data.csv")
 # print(round(monday_temp_F))
 #
 # Create a dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
 
-
-
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+# red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+# print(grey_squirrels_count)
+# print(red_squirrels_count)
+# print(black_squirrels_count)
+#
+# data_dict = {
+#     "Fur Color": ["Gray", "Cinnamon", "Black"],
+#     "Count:": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+# }
+# print(data_dict)
+# df = pandas.DataFrame(data_dict)
+# df.to_csv("squirrel_count.csv")
 
 
 
